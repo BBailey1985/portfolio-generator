0b18aff936477b6e18e2ef38a6561279a79b7483
BBailey1985 <brandon_bailey@hotmail.com>
2022-01-03 10:06:47 -0600

    updated templates to take display more data

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-const { link } = require('fs');
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw new Error (err);
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
-inquirer
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -62,15 +52,16 @@ const promptUser = () => {
 };
 
 const promptProject = portfolioData => {
-  // If there is so 'project array property, create one
-  if (!portfolioData.projects) {
-  portfolioData.projects = [];
-  }
-  console.log(`
+   console.log(`
 =================
 Add a New Project
 =================
 `);
+
+ // If there is so 'project array property, create one
+ if (!portfolioData.projects) {
+  portfolioData.projects = [];
+  }
   return inquirer.prompt([
     {
       type: 'input',
@@ -135,5 +126,11 @@ Add a New Project
 promptUser()
 .then(promptProject)
 .then(portfolioData => {
-  console.log(portfolioData);
+  const pageHTML = generatePage(portfolioData);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw new Error (err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
 });
